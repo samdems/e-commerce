90aa14eb1e26f5b57911178560fb01600dbb55ff
samdems <sam.cross@gmx.com>
2014-12-18 19:03:00 +0000

    css and html

--- index.php
+++ index.php
@@ -1,6 +1,135 @@
-<?php
-include 'config.php';
-echo substr($_SERVER["REQUEST_URI"], strlen ( $uri ));
+<!doctyle html>
+
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>	
+	<header>
+		<nav>
+			<ul>
+			  <li><a href="default.asp">Home</a></li>
+			  <li><a href="news.asp">News</a></li>
+			  <li><a href="contact.asp">Contact</a></li>
+			  <li><a href="about.asp">About</a></li>
+			</ul>
+		</nav>
+		<input class="left"></input>
+	</header>
+	<aside>		
+			<nav>
+				<ul id="CatButtons">
+				  <li><a href="default.asp">Home</a></li>
+				  <li><a href="news.asp">News</a></li>
+				  <li><a href="contact.asp">Contact</a></li>
+				  <li><a href="about.asp">About</a></li>
+				</ul>
+			</nav>
+		</aside>
+
+	<main>
+		<article>
+			<section class="large">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/440x220/ff00000" >
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+						<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>
+						<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+						<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>
+						<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>	
+
+			<section class="large">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/440x220/ff00000">
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>	
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000" >
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+			<section class="small">
+				<h1>title of iteam</h1>
+				<img src="http://placehold.it/200x100/ff00000">
+				<p>desciption</p>
+			</section>
+		</article>
 
 
-?>
+	</main>
+	<footer></footer>
+	<script src="js.js"></script>
+	</body>
+</html>
